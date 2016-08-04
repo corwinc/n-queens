af9c605b3ef4a5ec147b612d3127a554dea2d8f2
corwinc <corwin.crownover@gmail.com>
2016-08-04 12:13:11 -0700

    Fix solution pass

--- src/solvers.js
+++ src/solvers.js
@@ -42,22 +42,25 @@
 
 
 window.findNRooksSolution = function(n) {
-  var myBoard = new Board({'n': n});  //create a new board of N size to work with
+  var myBoard = new Board({n: n});  //create a new board of N size to work with
   var numPlaced = 0;  //keep track of how many rooks have been placed without conflict
+  var solution = [];
   var getRandomIndex = function() {  //make random number generator
     return Math.floor(Math.random() * n);  //get random x and y values
   };
 
   var placeAnX = function() {
+    debugger;
     var xCoord = getRandomIndex();
     var yCoord = getRandomIndex();  //need to optimize here --> if the pair has been checked already, generate new pair
     if (myBoard.get(xCoord)[yCoord] === 0) {  //check to see if there is an x there already
-      var curRow = board.get(xCoord);
-      curRow[yCoord] = 1;
-      board.set(xCoord, curRow);
+      var ogRow = myBoard.get(xCoord);
+      var newRow = Array.prototype.splice.apply(ogRow);
+      newRow[yCoord] = 1;
+      myBoard.set(xCoord, newRow);
     }
-    if (myBoard.hasAnyRowConflicts() || myBoard.hasAnyColConflicts) {  //if any conflicts, undo rook placement
-      myBoard.set(xCoord)[yCoord] = 0;
+    if (myBoard.hasAnyRowConflicts() || myBoard.hasAnyColConflicts()) {  //if any conflicts, undo rook placement
+      myBoard.set(xCoord, ogRow);
     } else {  //otherwise, leave the rook, and increment num placed
       numPlaced++;
     }
@@ -67,9 +70,13 @@ window.findNRooksSolution = function(n) {
     placeAnX();
   }
 
+  for (var i = 0; i < test.attributes.n; i++) {
+    solution.push(test.attributes[i]);
+  }
+
   //at this point we've place N rooks so we have found a solution
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(myBoard));
-  return myBoard;
+  return solution;
 };
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
