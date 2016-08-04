69e9ac2a01f6a88eb41c15578b0301b534832478
corwinc <corwin.crownover@gmail.com>
2016-08-04 11:06:13 -0700

    Attempt to set random (x,y) coordinate

--- src/solvers.js
+++ src/solvers.js
@@ -13,31 +13,59 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+  /*var myBoard = new Board({'n': n});  //create a new board of N size to work with
+  var numPlaced = 0;  //keep track of how many rooks have been placed without conflict
+  var getRandomIndex = function() {  //make random number generator
+    return Math.floor(Math.random() * n);  //get random x and y values
+  };
+
+  var placeAnX = function() {
+    var checked = {}
+    var xCoord = getRandomIndex();
+    var yCoord = getRandomIndex();  //need to optimize here --> if the pair has been checked already, generate new pair
+    if (myBoard.get(xCoord)[yCoord] === 0) {  //check to see if there is an x there already
+      // var curArray = new Array(n);
+      // curArray = [0, 0, 0, 0];
+      // board.set(xCoord, [arra]
+      myBoard.set(xCoord)[yCoord] = 1;  //if not, make it a rook
+    }
+    if (myBoard.hasAnyRowConflicts() || myBoard.hasAnyColConflicts) {  //if any conflicts, undo rook placement
+      myBoard.set(xCoord)[yCoord] = 0;
+    } else {  //otherwise, leave the rook, and increment num placed
+      numPlaced++;
+    }
+  };
+
+  while (numPlaced < n) {  //as long as we haven't placed N rooks, keep trying to place an X
+    placeAnX();
+  }*/
 
 
 window.findNRooksSolution = function(n) {
-  // var myBoard = new Board({'n': n});  //create a new board of N size to work with
-  // var numPlaced = 0;  //keep track of how many rooks have been placed without conflict
-  // var getRandomIndex = function() {  //make random number generator
-  //   return Math.floor(Math.random() * n);  //get random x and y values
-  // };
+  var myBoard = new Board({'n': n});  //create a new board of N size to work with
+  var numPlaced = 0;  //keep track of how many rooks have been placed without conflict
+  var getRandomIndex = function() {  //make random number generator
+    return Math.floor(Math.random() * n);  //get random x and y values
+  };
 
-  // var placeAnX = function() {
-  //   var xCoord = getRandomIndex();
-  //   var yCoord = getRandomIndex();  //need to optimize here --> if the pair has been checked already, generate new pair
-  //   if (myBoard.get(xCoord)[yCoord] === 0) {  //check to see if there is an x there already
-  //     myBoard.set(xCoord)[yCoord] = 1;  //if not, make it a rook
-  //   }
-  //   if (myBoard.hasAnyRowConflicts() || myBoard.hasAnyColConflicts) {  //if any conflicts, undo rook placement
-  //     myBoard.set(xCoord)[yCoord] = 0;
-  //   } else {  //otherwise, leave the rook, and increment num placed
-  //     numPlaced++;
-  //   }
-  // };
+  var placeAnX = function() {
+    var xCoord = getRandomIndex();
+    var yCoord = getRandomIndex();  //need to optimize here --> if the pair has been checked already, generate new pair
+    if (myBoard.get(xCoord)[yCoord] === 0) {  //check to see if there is an x there already
+      var curRow = board.get(xCoord);
+      curRow[yCoord] = 1;
+      board.set(xCoord, curRow);
+    }
+    if (myBoard.hasAnyRowConflicts() || myBoard.hasAnyColConflicts) {  //if any conflicts, undo rook placement
+      myBoard.set(xCoord)[yCoord] = 0;
+    } else {  //otherwise, leave the rook, and increment num placed
+      numPlaced++;
+    }
+  };
 
-  // while (numPlaced < n) {  //as long as we haven't placed N rooks, keep trying to place an X
-  //   placeAnX();
-  // }
+  while (numPlaced < n) {  //as long as we haven't placed N rooks, keep trying to place an X
+    placeAnX();
+  }
 
   //at this point we've place N rooks so we have found a solution
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(myBoard));
